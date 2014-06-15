ff99ef2860f0564f18c4819516c97eb1c63dc67c
ruben <demuynck_ruben@hotmail.com>
2014-06-15 15:31:04 +0200

    added new unimplemented excercise

---
+++ src/be/rd/structures/Exercise.java
@@ -0,0 +1,23 @@
+package be.rd.structures;
+
+/**
+ * Created by ruben on 6/15/14.
+ */
+public class Exercise {
+
+    public static void main(String[] args)
+    {
+        new Exercise().start();;
+    }
+
+
+    private String charsToSortByOccurence = "abcdefghijklmnopqrstuvwxyz_";
+
+
+    private void start()
+    {
+
+    }
+
+
+}
