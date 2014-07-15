27e58bbf50d24c68eb0fdd52e3a5ddcac234fede
ruben056 <demuynck_ruben@hotmail.com>
2014-07-15 12:00:11 +0200

    added excercise : Reverse every letter in a string but leave punctuation marks and everything else where it is. - add all chars to linked list until a punctuation char is found, then reverse the linked list and att the punctuation char - if there was no linked list (for instance sequential punctuation chars) just add the puncutation char to the result

---
+++ src/be/rd/structures/linkedlist/ReverseLetterPerword.java
@@ -0,0 +1,61 @@
+package be.rd.structures.linkedlist;
+
+/**
+ * Reverses each word but keeps the order of the words.
+ * Also leaves the punctuation marks as is.
+ */
+public class ReverseLetterPerWord
+{
+    private final static String INPUT = "blabla sdfqsdf.\n slmqdfj!! qsdfqsdf.";
+
+    public static void main(String[] args){
+        new ReverseLetterPerWord().test();
+    }
+
+    public void test(){
+        System.out.println(INPUT);
+        System.out.println("reverse per word:");
+        System.out.println(reverseLettersPerWord(INPUT));
+    }
+
+    /**
+     * Uses linked list because we don't know how big each list will be
+     * (arraylist would have to reallocate memory all the time...)
+     * @param s
+     * @return
+     */
+    public String reverseLettersPerWord(String s){
+
+        StringBuilder sb = new StringBuilder();
+
+        SimpleNode head = null;
+        SimpleNode prev = null;
+        for(char c : s.toCharArray()){
+
+            if(!Character.isLetter(c)){
+                // reverse linkedlist
+                if(head != null){
+                    head = Reverse.reverseIterative(head);
+                    sb.append(head.toString());
+                    head = null;
+                    prev = null;
+                }
+                sb.append(c);
+                continue;
+            }
+
+            // as long as it is not a character build a linked list so we can
+            // reverse it as soon as we encounter a char
+            SimpleNode cur = new SimpleNode(c, prev, null);
+            if(prev != null){
+                prev.next = cur;
+            }
+            else{
+                head = cur;
+            }
+            prev = cur;
+        }
+
+        return sb.toString();
+    }
+}
